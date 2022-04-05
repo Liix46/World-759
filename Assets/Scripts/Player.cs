8bb04cdc902e58b041a557c77e3e458416652400
Liix46 <boychev46@gmail.com>
2022-04-06 00:27:01 +0300

    @Liix46 add hw 5.04.2022

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,22 +6,51 @@ public class Player : MonoBehaviour
     public const int CAM_V_FACTOR = 2;
     public Camera cam;
 
-    private Vector3 rod;
-    private Vector3 camAngels;
+    private Vector3 _rod;
+    private Vector3 _camAngels;
+    private Vector3 _ccMove;
+
+    private CharacterController _characterController;
+    private float _camStartAngleY;
+    private float _characterSpeed;
 
     void Start()
     {
-        camAngels = Vector3.zero;
+        _camAngels = Vector3.zero;
+        _characterController = GetComponent<CharacterController>();
+        _rod = _characterController.transform.position - cam.transform.position;
+        _camStartAngleY = cam.transform.root.eulerAngles.y;
+
+        _characterSpeed = 3;
     }
 
+    [System.Obsolete]
     void Update()
     {
+
         //float dH = Input.GetAxis("Mouse X") * CAM_H_FACTOR;
         //float dV = Input.GetAxis("Mouse Y") * CAM_V_FACTOR;
         //cam.transform.Rotate(-dV, dH, 0);
 
-        camAngels.y += Input.GetAxis("Mouse X") * CAM_H_FACTOR;
-        camAngels.x -= Input.GetAxis("Mouse Y") * CAM_V_FACTOR;
-        cam.transform.eulerAngles = camAngels;
+        _camAngels.y += Input.GetAxis("Mouse X") * CAM_H_FACTOR;
+        _camAngels.x -= Input.GetAxis("Mouse Y") * CAM_V_FACTOR;
+        cam.transform.eulerAngles = _camAngels;
+
+        cam.transform.position = _characterController.transform.position -
+            (Quaternion.EulerAngles(0, _camAngels.y - _camStartAngleY, 0) * _rod);
+
+        // Character move
+        _ccMove = (cam.transform.right * Input.GetAxis("Horizontal"))
+            + (cam.transform.forward * Input.GetAxis("Vertical"));
+        _ = _characterController.SimpleMove(_ccMove * _characterSpeed);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale =
+                Time.timeScale == 0
+                ? 1
+                : 0;
+            Debug.Log(Time.timeScale);
+        }
     }
 }
